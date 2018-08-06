045a0ab3da35057bf73898e633fd8ddafb17cd4d
sofiajhk <sofiajihyek@gmail.com>
2018-08-06 14:45:15 -0500

    updated assignments

--- ANGULAR/quoteRanks/src/app/app.component.ts
+++ ANGULAR/quoteRanks/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { QuoteComponent } from './quote/quote.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Add a Quote';
+  newQuote = new QuoteComponent();
+  // newQuote = { quote: '', author: '', rating: 0 };
+  quotes = [];
+  submitted;
+  
+  onSubmit(myForm) {
+    console.log("it works");
+    console.log(this.newQuote);
+    this.quotes.push(this.newQuote);
+    this.submitted = this.newQuote;
+    this.newQuote = new QuoteComponent();
+    console.log(this.quotes);
+    // this.newQuote = { quote: '', author: '', rating: 0 };
+  }
 }
--- ANGULAR/quoteRanks/src/app/app.module.ts
+++ ANGULAR/quoteRanks/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { HttpModule } from '@angular/http'; // Import HttpModule
 
 import { AppComponent } from './app.component';
+import { QuoteListComponent } from './quote-list/quote-list.component';
+import { QuoteComponent } from './quote/quote.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteListComponent,
+    QuoteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, // Include module in our AppModules
+    HttpModule // Include module in our AppModules
   ],
   providers: [],
   bootstrap: [AppComponent]
